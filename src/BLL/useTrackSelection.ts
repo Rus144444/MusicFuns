38b67f12bd225f48a9ef457f25ee5cf1f875c117
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-02 08:51:22 +0200

    src/BLL/useTrackSelection.ts fix bag

--- src/BLL/useTrackSelection.ts
+++ src/BLL/useTrackSelection.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { getTrack } from "../DAL/api-fake"
 import type { SelectedTrackType, ApiResponse } from "../DAL/api"
 
-export function useTrackSelection (selectedTrackId) {
+export function useTrackSelection (selectedTrackId: string| null) {
   const [selectedTrack, setSelectedTrack] = useState<SelectedTrackType|null>(null)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
